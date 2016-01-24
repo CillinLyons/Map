b671472b7c7cb6c343894f2fffbabab2e7d4bb2c
CillinLyons <cillinlyons@outlook.com>
2016-01-24 18:55:52 +0000

    Main menu has been created with 2 options: 1)Map 2)Crime Report

--- app/src/main/java/com/example/cillin/map/MainMenu.java
+++ app/src/main/java/com/example/cillin/map/MainMenu.java
@@ -1,9 +1,61 @@
 package com.example.cillin.map;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 /**
  * Created by Cillin on 22/01/2016.
  */
-public class MainMenu
+public class MainMenu extends Activity
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+
+        populateMenu();
+        changePage();
+    }
+
+    private void populateMenu()
+    {
+        //Create items for the menu
+        String[] menuItems = {"Map", "Crime Report"};
+
+        //Create adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.main_menu_items, menuItems);
+
+        //Configure menu
+        ListView menu = (ListView) findViewById(R.id.menu);
+        menu.setAdapter(adapter);
+    }
+
+    private void changePage()
+    {
+        ListView menu = (ListView) findViewById(R.id.menu);
+        menu.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id)
+            {
+                switch(position)
+                {
+                    case 0:  Intent firstActivity = new Intent(MainMenu.this, MapsActivity.class);
+                        startActivity(firstActivity);
+                        break;
+                    //case 1: Intent secondActivity = new Intent(MainMenu.this, );
+
+                }
+            }
+        });
+    }
 
 }
